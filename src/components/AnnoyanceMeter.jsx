8b3144f2801927a7a4ac4401458524384292d9a3
Brittani Ericksen <heybrittani@gmail.com>
2020-10-30 00:12:24 -0400

    fixing bugs

--- src/components/AnnoyanceMeter.jsx
+++ src/components/AnnoyanceMeter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from "react-redux";
 import { setLevel } from "../redux/actions";
 import WouldRatherButton from "./WouldRatherButton";
